a16c06463269c712f309c1f01ad18f8530151dbd
ElectroSpyder <prg@live.com.ar>
2020-09-07 23:51:26 -0300

    Primer aplicacion Xamarin corriendo

---
+++ xa.Turno/xa.Turno.App/xa.Turno.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using xa.Turno.App.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xa.Turno.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ListadoPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
